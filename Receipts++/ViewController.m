2025f52f23e77a2b972b2b097fa3ed09e6996157
Erin Luu <luuerin@gmail.com>
2016-11-24 11:06:41 -0500

    Initial Commit

---
+++ Receipts++/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Receipts++
+//
+//  Created by Erin Luu on 2016-11-24.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
